56242c83418a45ba376f0f6e41b789bf08e31266
Justin <justin@oneadvanced.com>
2017-09-27 17:19:14 +0530

    added image upload functionality

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,5 @@
 class PhotosController < ApplicationController
+ 
  #Index action, photos gets listed in the order at which they were created
  def index
   @photos = Photo.order('created_at')
@@ -7,16 +8,17 @@ class PhotosController < ApplicationController
  #New action for creating a new photo
  def new
   @photo = Photo.new
-  @categories = Category.all
+  @category_options = Category.all.map{|u|[u.name, u.id]}
  end
 
  #Create action ensures that submitted photo gets created if it meets the requirements
  def create
-  @categories = Category.all
+   @category_options = Category.all.map{|u|[u.name, u.id]}
   @photo = Photo.new(photo_params)
+
   if @photo.save
    flash[:notice] = "Successfully added new photo!"
-   redirect_to root_path
+   redirect_to "/photos"
   else
    flash[:alert] = "Error adding new photo!"
    render :new
@@ -29,4 +31,5 @@ class PhotosController < ApplicationController
  def photo_params
   params.require(:photo).permit(:category_id, :title, :image)
  end
+
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,16 +1,25 @@
 class Photo < ApplicationRecord
   #Mounts paperclip image
-  has_attached_file :image
+  has_attached_file :image, 
+                    style: {
+                      thumb: ["150x150#", :jpeg],
+                      original: [:jpeg]
+                    },
+                    convert_options: {
+                      thumb: "-quality 70 -strip",
+                      original: "-quality 90"
+                    }
   
   #This validates the type of file uploaded. According to this, only images are allowed.
-  #validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  #validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   #do_not_validate_attachment_file_type :image
   
   
   #Validates file, file type and file size
-  validates_attachment :image, presence: true,
-  content_type: { content_type: /\Aimage\/.*\z/ },
-  size: { in: 0..50.kilobytes }
+  #validates_attachment :image, presence: true,
+  #content_type: { content_type: /\Aimage\/.*\z/ },
+  #size: { in: 0..50.kilobytes }
   
   belongs_to :category
 end
