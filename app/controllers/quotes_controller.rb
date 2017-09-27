9227a0e1adf41e3e13f74483111daa086b684b2c
Justin <justin@oneadvanced.com>
2017-09-27 10:29:20 +0530

    provided category options for category drop down

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,11 +1,11 @@
 class QuotesController < ApplicationController
-  before_action :set_quote, :set_categories, only: [:show, :edit, :update, :destroy]
+  before_action :set_quote, only: [:show, :edit, :update, :destroy]
+  
   
   # GET /quotes
   # GET /quotes.json
   def index
     @quotes = Quote.all
-   
   end
 
   # GET /quotes/1
@@ -16,17 +16,21 @@ class QuotesController < ApplicationController
   # GET /quotes/new
   def new
     @quote = Quote.new
+    @category_options = Category.all.map{|u|[u.name, u.id]}
+    
   end
 
   # GET /quotes/1/edit
   def edit
+    @category_options = Category.all.map{|u|[u.name, u.id]}
   end
 
   # POST /quotes
   # POST /quotes.json
   def create
+    @category_options = Category.all.map{|u|[u.name, u.id]}
     @quote = Quote.new(quote_params)
-    @quote.category_id = 1
+    #@quote.category_id = 1
     respond_to do |format|
       if @quote.save
         format.html { redirect_to @quote, notice: 'Quote was successfully created.' }
@@ -67,13 +71,9 @@ class QuotesController < ApplicationController
     def set_quote
       @quote = Quote.find(params[:id])
     end
-    
-    def set_categories
-      @categories = Category.all
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def quote_params
-      params.require(:quote).permit(:name)
+      params.require(:quote).permit(:name, :category_id)
     end
 end
